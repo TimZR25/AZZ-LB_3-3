a6806a75bcfd3ebf490520410d866fb6e21ed09a
Zart <Timon_Z@inbox.ru>
2023-11-12 16:02:13 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AZZ_LB_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
